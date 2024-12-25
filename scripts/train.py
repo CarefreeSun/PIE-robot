21be79bb48877b815e45a0c82d0924bcb08fdccf
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-12-25 16:10:21 +0800

    Update

--- scripts/train.py
+++ scripts/train.py
@@ -79,9 +79,6 @@ def main():
     except:
         pass
 
-    # from huggingface_hub import login
-    # login(token='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')
-
     parser = H4ArgumentParser((ModelArguments, DataArguments, SFTConfig))
     model_args, data_args, training_args = parser.parse()
 
