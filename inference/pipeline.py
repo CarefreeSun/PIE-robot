b1512770fb7ce8e6e468aa0d7a95070e0272e428
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-12-31 16:43:09 +0800

    brutal force for loading checkpoint

--- inference/pipeline.py
+++ inference/pipeline.py
@@ -82,6 +82,7 @@ def call_vla(instance_data: dict, processor: AutoProcessor, tokenizer: AutoToken
     generate_ids = vla_pipe.generate(**input_data, eos_token_id=processor.tokenizer.eos_token_id, **generation_args)
     # remove input tokens 
     generate_ids = generate_ids[:, input_data['input_ids'].shape[1]:]
+    print(generate_ids)
     output_text = tokenizer.batch_decode(generate_ids, skip_special_tokens=False, clean_up_tokenization_spaces=False)[0]
 
     print(output_text)
